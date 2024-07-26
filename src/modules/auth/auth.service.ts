cff5695b84b11f83c3067ef632c61f841f6728cb
milad-taghizadeh <miladtaghizadeh1382@outlook.com>
2024-07-26 14:09:32 +0330

    feat:creating random username and otp code bugs fixed

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -11,6 +11,7 @@ import { isEmail, isPhoneNumber } from 'class-validator';
 import { PrismaService } from 'src/database/database.service';
 import { User } from '@prisma/client';
 import { AuthMessage, BadRequestMessage } from 'src/common/enums/message.enum';
+import { randomInt } from 'crypto';
 
 @Injectable()
 export class AuthService {
@@ -28,15 +29,44 @@ export class AuthService {
   }
 
   async login(method: AuthMethod, username: string) {
-    const validUsername = this.usernameValidator(method, username);
+    const validUsername = await this.usernameValidator(method, username);
     const user: User = await this.checkExistUser(method, validUsername);
     if (!user) throw new UnauthorizedException(AuthMessage.NotFoundAccount);
+    const otp = await this.saveOtp(user.id);
+    otp.userId = user.id;
+    return { code: otp.code };
   }
 
   async register(method: AuthMethod, username: string) {
-    const validUsername = this.usernameValidator(method, username);
-    const user: User = await this.checkExistUser(method, validUsername);
+    const validUsername = await this.usernameValidator(method, username);
+    let user: User = await this.checkExistUser(method, validUsername);
+    const randomUsername: string = `m_${Date.now()}`;
     if (user) throw new ConflictException(AuthMessage.AlreadyExistAccount);
+    if (method === AuthMethod.Username) {
+      throw new BadRequestException(BadRequestMessage.InvalidRegisterData);
+    }
+    user = await this.databaseService.user.create({
+      data: { [method]: username, username: randomUsername },
+    });
+    const otp = await this.saveOtp(user.id);
+    otp.userId = user.id;
+    return { code: otp.code };
+  }
+
+  async saveOtp(userId: string) {
+    const code = randomInt(10000, 99999).toString();
+    const expiresIn = new Date(Date.now() + 1000 * 2 * 60);
+    let otp = await this.databaseService.otp.findFirst({ where: { userId } });
+    if (otp) {
+      otp.code = code;
+      otp.expiresIn = expiresIn;
+    } else {
+      otp = await this.databaseService.otp.create({
+        data: { code, expiresIn, userId },
+      });
+    }
+    // Send SMS or EMAIL for user
+    return otp;
   }
 
   async checkOtp() {}
@@ -58,7 +88,7 @@ export class AuthService {
     } else throw new BadRequestException(BadRequestMessage.InvalidLoginData);
     return user;
   }
-  usernameValidator(method: AuthMethod, username: string) {
+  async usernameValidator(method: AuthMethod, username: string) {
     switch (method) {
       case AuthMethod.Email:
         if (isEmail(username)) return username;
