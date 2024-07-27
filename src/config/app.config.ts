3cbca84ee31bef10348201e363fa6da91bc3ad37
milad-taghizadeh <miladtaghizadeh1382@outlook.com>
2024-07-27 09:22:06 +0330

    UPDATE: swagger configs updated

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -8,6 +8,10 @@ export enum ConfigKeys {
 const AppConfig = registerAs(ConfigKeys.App, () => ({
   SERVER_PORT: 3000,
   SWAGGER_ENDPOINT: 'api-doc',
+  SWAGGER_TITLE: 'Virgool-Backend',
+  SWAGGER_DESCRIPTION: 'Api endpoints of virgool website',
+  SWAGGER_VERSION: '1.2',
+  SWAGGER_ADD_TAG: 'TEST',
 }));
 
 export const configurations = [AppConfig];
